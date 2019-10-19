5bdfe507264d674893e84ee7d04375af2efb91c9
maccza <mc180996@wp.pl>
2019-10-19 20:59:09 +0200

    add buttons

--- GUI/MainInterface.py
+++ GUI/MainInterface.py
@@ -2,6 +2,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import XML.ParseWindowSettings as ParseWindowSettings
+import XML.ParseButtonsSettings as ParseButtonsSettings
 
 class MainInterface(Gtk.Window):
     def __init__(self, *args, **kwargs):
@@ -12,10 +13,35 @@ class MainInterface(Gtk.Window):
         self.read_window_settings()
         self.set_size_request(self.width,self.height)
         self.set_position(Gtk.WindowPosition.CENTER)
-        self.set_title(self.widow_title)  
+        self.set_title(self.widow_title) 
+        self.panel_init()
     def read_window_settings(self):
         parse_xml = ParseWindowSettings.ParseWindowSettings()
         self.width = parse_xml.return_width()
         self.height = parse_xml.return_height()
         self.widow_title = parse_xml.return_window_title()
-     
+    def panel_init(self):
+        self.main_box = Gtk.Box()
+        self.add(self.main_box)
+        self.button_box = Gtk.Grid()
+        self.charts_box = Gtk.Grid()
+        self.main_box.add(self.button_box)
+        self.main_box.add(self.charts_box)
+        self.button_panel_init()
+    
+    def button_panel_init(self):
+        self.start_button()
+        self.stop_button()
+        self.button_box.add(self.start_button_)
+        self.button_box.attach_next_to(self.stop_button_,self.start_button_,Gtk.PositionType.BOTTOM,2,2)
+
+    def start_button(self):
+        self.start_button_ = Gtk.Button(label = 'Start')
+        parse_xml = ParseButtonsSettings.ParseButtonSettings()
+        parse_xml.parse_button_settings_by_name('Start')
+        self.start_button_.set_size_request(parse_xml.return_width(),parse_xml.return_height())   
+    def stop_button(self):
+        self.stop_button_ = Gtk.Button(label = 'Stop')
+        parse_xml = ParseButtonsSettings.ParseButtonSettings()
+        parse_xml.parse_button_settings_by_name('Stop')
+        self.stop_button_.set_size_request(parse_xml.return_width(),parse_xml.return_height())
---
+++ XML/ParseButtonsSettings.py
@@ -0,0 +1,20 @@
+from xml.dom import minidom
+class ParseButtonSettings:
+    def __init__(self):
+        # xml_ = minidom.parse('XML/Button_settings.xml')
+        self.xml_ = minidom.parse('XML/Button_settings.xml')
+        self.settings_tree = self.xml_.documentElement
+        self.settings = self.settings_tree.getElementsByTagName('Button')
+    def parse_button_settings_by_name(self,name):
+        for setting in self.settings:
+            if  setting.getAttribute('name') == name:
+                par_width= setting.getElementsByTagName('width')[0]
+                self.width= int(par_width.childNodes[0].data)
+                par_height = setting.getElementsByTagName('height')[0]
+                self.height = int(par_height.childNodes[0].data)
+    
+    def return_height(self):
+        return self.height
+    def return_width(self):
+        return self.width
+
